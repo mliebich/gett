48edf092bc74aee56de8d69a571f5b24853585f4
mliebich <michi.liebich@gmail.com>
2018-02-19 16:48:55 +0000

    add login form

--- gettapp/routes.py
+++ gettapp/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from gettapp import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -7,12 +8,17 @@ def index():
     user = {'username': 'michi'}
     posts = [
         {
-                'author': {'username': 'michael'}
+                'author': {'username': 'michael'},
                 'body': 'I love chemistry'
          },
         {
-                'author': {'username': 'mike'}
+                'author': {'username': 'mike'},
                 'body': 'I love math'
         }
     ]
     return render_template('index.html', title='home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
